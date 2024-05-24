d3302132b5af52f6a87767195c4abff2ee684eb4
AnaCvetkovik <164337285+AnaCvetkovik@users.noreply.github.com>
2024-05-24 16:29:18 +0200

    Add files via upload

---
+++ SI_lab2_223174/src/main/java/com/example/si_lab2_223174/SiLab2223174Application.java
@@ -0,0 +1,13 @@
+package com.example.si_lab2_223174;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiLab2223174Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiLab2223174Application.class, args);
+    }
+
+}
---
+++ SI_lab2_223174/src/test/java/com/example/si_lab2_223174/SiLab2223174ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.si_lab2_223174;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiLab2223174ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
